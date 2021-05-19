1560086c55cdfc5b64b9b83a2db76d8c8629ad38
Irokanade <michaeljs.leong@gmail.com>
2021-05-19 11:05:39 +0800

    Initial Commit

---
+++ san_juan/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  san_juan
+//
+//  Created by michaelleong on 19/05/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
